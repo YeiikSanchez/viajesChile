9b2ea2aa2eab3fd39fc33e6f4d35c1a93aa915f9
yeminsons <yeminsons@gmail.com>
2022-02-05 00:33:47 -0300

    five commit add icon and animate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
 // COMENTARIOS/
-alert(hola);
+$(document).ready(function () {
+  $('[data-bs-toggle="tooltip"]').tooltip();
+
+  $(".scroll").click(function () {
+    var gato = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(gato).offset().top - 70,
+      },
+      800
+    );
+  });
+});
